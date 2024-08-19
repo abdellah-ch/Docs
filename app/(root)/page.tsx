1120b5e46006c2d6d0f372ddfd3d384cdd8e6d27
abdellah-ch <abdellahchotta55@gmail.com>
2024-08-19 15:47:34 +0100

    m

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,7 +7,6 @@ import Link from "next/link";
 import Header from "@/components/Header";
 import AddDocumentBtn from "@/components/AddDocumentBtn";
 
-
 const Home = async () => {
   const clerkUser = await currentUser();
   if (!clerkUser) redirect("/sign-in");
@@ -26,8 +25,15 @@ const Home = async () => {
         </div>
       </Header>
       {roomDocuments.data.length > 0 ? (
-        <div>
-          <p>data</p>{" "}
+        <div className="flex flex-col items-center mb-10 w-full gap-10 px-5">
+          <div className="max-w-[730px] items-end flex w-full justify-between">
+            <h3 className="text-28-semibold">All documents</h3>
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+          <p>data</p>
         </div>
       ) : (
         <div className="document-list-empty">
@@ -39,7 +45,10 @@ const Home = async () => {
             className="mx-auto"
           />
 
-          <AddDocumentBtn userId = {clerkUser.id} email = {clerkUser.emailAddresses[0].emailAddress}  />
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
         </div>
       )}
     </main>
